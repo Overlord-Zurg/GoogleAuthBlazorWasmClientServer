2700af135077e757c973c93ffd034100773ee96f
Ben Lappin <Ben.S.Lappin@gmail.com>
2025-04-06 14:21:16 -0500

    instead, do it right

--- src/GoogleAuthBlazorWasmClientServer/Client/Program.cs
+++ src/GoogleAuthBlazorWasmClientServer/Client/Program.cs
@@ -8,24 +8,20 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddOidcAuthentication(options =>
-{
-    // Configure your authentication provider options here.
-    // For more information, see https://aka.ms/blazor-standalone-auth
-    builder.Configuration.Bind("Local", options.ProviderOptions);
-});
-
 var googleClientID = "GOOGLE_CLIENT_ID_GOES_HERE";
 
 builder.Services.AddOidcAuthentication(options =>
 {
+    // Configure your authentication provider options here.
+    // For more information, see https://aka.ms/blazor-standalone-auth
     options.ProviderOptions.ClientId = googleClientID;
     options.ProviderOptions.Authority = "https://accounts.google.com";
-    options.ProviderOptions.RedirectUri = "https://localhost:5001/authentication/login-callback";
-    options.ProviderOptions.ResponseType = "code";
-    options.ProviderOptions.DefaultScopes.Add("openid");
-    options.ProviderOptions.DefaultScopes.Add("profile");
-    options.ProviderOptions.DefaultScopes.Add("email");
+    options.ProviderOptions.RedirectUri = "https://localhost:7135/authentication/login-callback";
+    options.ProviderOptions.ResponseType = "id_token";
+    //options.ProviderOptions.DefaultScopes.Add("openid");
+    //options.ProviderOptions.DefaultScopes.Add("profile");
+    //options.ProviderOptions.DefaultScopes.Add("email");
 });
+
 
 await builder.Build().RunAsync();
